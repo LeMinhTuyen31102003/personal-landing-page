504059119a3787e27a6fa0735160816a0cb01d0f
LeMinhTuyen31102003 <leminhtuyen1111@gmail.com>
2026-06-08 14:02:03 +0700

    fead: add speed insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 <Analytics />;
+<SpeedInsights />;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
